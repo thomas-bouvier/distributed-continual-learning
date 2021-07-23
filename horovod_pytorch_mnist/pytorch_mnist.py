d30a3c0eb833d6c7dfe22f5f080dbba9463c0eff
Thomas Bouvier <contact@thomas-bouvier.io>
2021-07-23 18:25:16 +0200

    Create package for models

--- horovod_pytorch_mnist/pytorch_mnist.py
+++ horovod_pytorch_mnist/pytorch_mnist.py
@@ -14,23 +14,13 @@ from torchvision import datasets, transforms
 import torch.utils.data.distributed
 import horovod.torch as hvd
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+import models
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
+from torchsummary import summary
+
+model_names = sorted(name for name in models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(models.__dict__[name]))
 
 class Experiment():
     def __init__(self, args, kwargs):
@@ -75,7 +65,8 @@ class Experiment():
 
 
     def _create_model(self):
-        self.model = Net()
+        self.model = models.__dict__['mnist_net']()
+        summary(self.model, (1, 28, 28))
 
         # By default, Adasum doesn't need scaling up learning rate.
         lr_scaler = hvd.size() if not self.args.use_adasum else 1
---
+++ models/mnistnet.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class mnist_net(nn.Module):
+    def __init__(self):
+        super(mnist_net, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(True),
+
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(p=0.25),
+            nn.Flatten(),
+
+            nn.Linear(in_features=9216, out_features=128),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+
+            nn.Linear(in_features=128, out_features=10),
+
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        return self.features(x)
+
+
+def mnist(**kwargs):
+    return mnist_net()
