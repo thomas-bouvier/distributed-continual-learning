ae2f9ea1b56677a2fccb675f5d83b735f597df65
Thomas Bouvier <contact@thomas-bouvier.io>
2022-04-11 16:55:31 +0200

    fix(nil_global): loop() from parent was called

--- agents/nil_global.py
+++ agents/nil_global.py
@@ -1,16 +1,17 @@
 import horovod.torch as hvd
 import math
 import numpy as np
-import numpy.ma as ma
 import logging
 import random
 import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torchvision
 
 from agents.base import Agent
 from utils.utils import get_device, move_cuda
+from utils.meters import AverageMeter, accuracy
 
 class nil_global_agent(Agent):
     def __init__(self, model, config, optimizer, criterion, cuda, log_interval, state_dict=None):
@@ -19,15 +20,15 @@ class nil_global_agent(Agent):
         if state_dict is not None:
             self.model.load_state_dict(state_dict)
 
-        self.num_reps = 0
-        self.num_representatives = config.get('num_representatives', 60)   # number of stored examples per class
+        self.num_representatives = config.get('num_representatives', 60)
         self.num_candidates = config.get('num_candidates', 20)
         self.batch_size = config.get('batch_size')
 
-        self.local_reps = [torch.Tensor() for _ in range(model.num_classes)]
-        self.local_candidates = [torch.Tensor() for _ in range(model.num_classes)]
+        self.num_reps = 0
+        self.representatives = [torch.Tensor() for _ in range(model.num_classes)]
+        self.candidates = [torch.Tensor() for _ in range(model.num_classes)]
         self.reps_by_worker = [[torch.Tensor() for _ in range(model.num_classes)] for _ in range(hvd.size())]
-        self.class_count = [0 for _ in range(model.num_classes)]
+        self.class_count = torch.tensor([0 for _ in range(model.num_classes)])
 
         self.global_x_reps = None
         self.global_y_reps = None
@@ -35,7 +36,7 @@ class nil_global_agent(Agent):
 
         self.mask = torch.as_tensor([0.0 for _ in range(self.model.num_classes)], device=torch.device(get_device(self.cuda)))
 
-    def get_candidates(self):
+    def get_representatives(self):
         """Select or retrieves the representatives from the data
 
         :return: a list of num_candidates representatives.
@@ -59,90 +60,82 @@ class nil_global_agent(Agent):
         :param megabatch: current megabatch
         :return: None
         """
-        rand_indices = torch.randperm(len(y))
-        x = x.clone()[rand_indices]
-        y = y.clone()[rand_indices]
         i = min(self.num_candidates, len(x))
-        x = x[:i]
-        y = y[:i]
+        rand_indices = torch.randperm(len(y))
+        x = x.clone()[rand_indices][:i]
+        y = y.clone()[rand_indices][:i]
         labels, counts = y.unique(return_counts=True)
         for i in range(len(labels)):
             self.class_count[labels[i]] += counts[i]
-            self.local_candidates[labels[i]] = x[(y == labels[i]).nonzero(as_tuple=True)[0]]
+            self.candidates[labels[i]] = x[(y == labels[i]).nonzero(as_tuple=True)[0]]
 
-        removed = []
-        added = []
+        rm = []
+        add = []
         offsets = [0]
         for i in range(self.model.num_classes):
-            offsets.append(offsets[-1] + len(self.local_candidates[i]))
-            rand_indices = torch.randperm(len(self.local_reps[i]) + len(self.local_candidates[i]))[:self.num_representatives]
-            removed += [j + self.num_representatives * i for j in range(len(self.local_reps[i])) if j not in rand_indices]
-            added += [j + self.num_candidates * i for j in range(len(self.local_candidates[i])) if j + len(self.local_reps[i]) in rand_indices]
+            offsets.append(offsets[-1] + len(self.candidates[i]))
+            rand_indices = torch.randperm(len(self.representatives[i]) + len(self.candidates[i]))[:self.num_representatives]
+            rm += [j + self.num_representatives * i for j in range(len(self.representatives[i])) if j not in rand_indices]
+            add += [j + self.num_candidates * i for j in range(len(self.candidates[i])) if j + len(self.representatives[i]) in rand_indices]
 
-        while len(added) < self.model.num_classes * self.num_candidates:
-            added.append(-1)
-        while len(removed) < self.model.num_classes * self.num_candidates:
-            removed.append(-1)
+        while len(add) < self.model.num_classes * self.num_candidates:
+            add.append(-1)
+        while len(rm) < self.model.num_classes * self.num_candidates:
+            rm.append(-1)
 
-        self.share_reps(removed, added, offsets)
+        self.share_reps(rm, add, offsets)
         self.recalculate_weights()
         self.num_reps = len(self.global_y_reps)
 
-    def share_reps(self, removed, added, offsets):
-        """
-
+    def share_reps(self, rm, add, offsets):
+        """Share representatives accross workers
         """
         if sum(self.class_count) == 0:
             return
-
         self.global_class_count = hvd.allreduce(self.class_count, op=hvd.Sum)
-
-        removed = torch.tensor([removed])
-        removed = hvd.allgather(removed)
-        added = torch.tensor([added])
-        added = hvd.allgather(added)
+        rm = torch.tensor([rm])
+        rm = hvd.allgather(rm)
+        add = torch.tensor([add])
+        add = hvd.allgather(add)
         offsets = torch.tensor([offsets])
         offsets = hvd.allgather(offsets)
-        new_reps = hvd.allgather(torch.cat(self.local_candidates).unsqueeze(0))
+        new_reps = hvd.allgather(torch.cat(self.candidates).unsqueeze(0))
 
         for worker in range(hvd.size()):  
-            to_add_w = added[worker] 
-            to_add_w = to_add_w[to_add_w != -1]
-            to_rm_w = removed[worker]
-            to_rm_w = to_rm_w[to_rm_w != -1]
+            add_w = add[worker] 
+            add_w = add_w[add_w != -1]
+            rm_w = rm[worker]
+            rm_w = rm_w[rm_w != -1]
 
             for c in range(self.model.num_classes):
                 lower = offsets[worker][c]
                 upper = offsets[worker][c + 1]
-
                 if lower == upper:
                     continue
 
-                to_add = to_add_w[to_add_w >= self.n_candidates * c]
-                to_add = to_add[to_add < self.n_candidates * (c + 1)]
-                to_add -= self.n_candidates * c
-
-                if len(to_rm_w) > 0:
-                    to_rm = to_rm_w[to_rm_w >= self.num_representatives * c]
-                    to_rm = to_rm[to_rm < self.num_representatives * (c + 1)]
+                to_add = add_w
+                to_add = to_add[(to_add >= self.num_candidates * c) & (to_add < self.num_candidates * (c + 1))]
+                to_add -= self.num_candidates * c
+                if len(rm_w) > 0:
+                    to_rm = rm_w
+                    to_rm = to_rm[(to_rm >= self.num_representatives * c) & (to_rm < self.num_representatives * (c + 1))]
                     to_rm -= self.num_representatives * c
 
                 if len(torch.flatten(self.reps_by_worker[worker][c])) == 0:
                     self.reps_by_worker[worker][c] = new_reps[worker][lower:upper][to_add]
                 else:
-                    if len(to_rm_w) > 0:
-                        if len(to_rm) > 0:
-                            selected = [i for i in range(len(self.reps_by_worker[worker][c])) if i not in to_rm]
-                            self.reps_by_worker[worker][c] = self.reps_by_worker[worker][c][selected]
+                    if len(rm_w) > 0 and len(to_rm) > 0:
+                        selected = [i for i in range(len(self.reps_by_worker[worker][c])) if i not in to_rm]
+                        self.reps_by_worker[worker][c] = self.reps_by_worker[worker][c][selected]
                     self.reps_by_worker[worker][c] = torch.cat((self.reps_by_worker[worker][c], new_reps[worker][lower:upper][to_add])) 
 
         self.global_x_reps = torch.cat([torch.cat(per_worker) for per_worker in self.reps_by_worker])
-        self.local_candidates = [torch.Tensor() for _ in range(self.model.num_classes)]
+        self.candidates = [torch.Tensor() for _ in range(self.model.num_classes)]
 
         for i in range(self.model.num_classes):
-            self.local_reps[i] = self.reps_by_worker[hvd.rank()][i] 
+            self.representatives[i] = self.reps_by_worker[hvd.rank()][i] 
 
-        y = torch.tensor([i for i in range(self.model.num_classes) for _ in range(len(self.local_reps[i]))])
+        y = torch.tensor([i for i in range(self.model.num_classes) for _ in range(len(self.representatives[i]))])
         self.global_y_reps = hvd.allgather(y)
 
     def recalculate_weights(self):
@@ -193,67 +186,67 @@ class nil_global_agent(Agent):
 
         self.criterion = nn.CrossEntropyLoss(weight=self.mask, reduction='none')
 
-        """
-        Forward pass for the current epoch
-        """
-        def loop(self, data_regime, average_output=False, training=False):
-            prefix='train' if training else 'val'
-            meters = {metric: AverageMeter(f"{prefix}_{metric}")
-                    for metric in ['loss', 'prec1', 'prec5']}
-            start = time.time()
-            step_count = 0
+    """
+    Forward pass for the current epoch
+    """
+    def loop(self, data_regime, average_output=False, training=False):
+        prefix='train' if training else 'val'
+        meters = {metric: AverageMeter(f"{prefix}_{metric}")
+                for metric in ['loss', 'prec1', 'prec5']}
+        start = time.time()
+        step_count = 0
 
-            for i_batch, (x, y, t) in enumerate(data_regime.get_loader()):
-                torch.cuda.nvtx.range_push(f"Batch {i_batch}")
+        for i_batch, (x, y, t) in enumerate(data_regime.get_loader()):
+            torch.cuda.nvtx.range_push(f"Batch {i_batch}")
 
-                output, loss = self._step(i_batch, x, y, training=training,
-                                        average_output=average_output)
+            output, loss = self._step(i_batch, x, y, training=training,
+                                    average_output=average_output)
 
-                # measure accuracy and record loss
-                prec1, prec5 = accuracy(output[:y.size(0)], y, topk=(1, min(self.model.num_classes, 5)))
-                meters['loss'].update(loss)
-                meters['prec1'].update(prec1, x.size(0))
-                meters['prec5'].update(prec5, x.size(0))
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(output[:y.size(0)], y, topk=(1, min(self.model.num_classes, 5)))
+            meters['loss'].update(loss)
+            meters['prec1'].update(prec1, x.size(0))
+            meters['prec5'].update(prec5, x.size(0))
 
-                if i_batch % self.log_interval == 0 or i_batch == len(data_regime.get_loader()):
-                    logging.info('{phase}: epoch: {0} [{1}/{2}]\t'
-                                'Loss {meters[loss].val:.4f} ({meters[loss].avg:.4f})\t'
-                                'Prec@1 {meters[prec1].val:.3f} ({meters[prec1].avg:.3f})\t'
-                                'Prec@5 {meters[prec5].val:.3f} ({meters[prec5].avg:.3f})\t'
-                                .format(
-                                    self.epoch+1, i_batch, len(data_regime.get_loader()),
-                                    phase='TRAINING' if training else 'EVALUATING',
-                                    meters=meters))
+            if i_batch % self.log_interval == 0 or i_batch == len(data_regime.get_loader()):
+                logging.info('{phase}: epoch: {0} [{1}/{2}]\t'
+                            'Loss {meters[loss].val:.4f} ({meters[loss].avg:.4f})\t'
+                            'Prec@1 {meters[prec1].val:.3f} ({meters[prec1].avg:.3f})\t'
+                            'Prec@5 {meters[prec5].val:.3f} ({meters[prec5].avg:.3f})\t'
+                            .format(
+                                self.epoch+1, i_batch, len(data_regime.get_loader()),
+                                phase='TRAINING' if training else 'EVALUATING',
+                                meters=meters))
 
-                    if self.writer is not None:
-                        self.writer.add_scalar(f"{prefix}_loss", meters['loss'].avg, self.global_steps)
-                        self.writer.add_scalar(f"{prefix}_prec1", meters['prec1'].avg, self.global_steps)
-                        self.writer.add_scalar(f"{prefix}_prec5", meters['prec5'].avg, self.global_steps)
-                        if training:
-                            self.writer.add_scalar('lr', self.optimizer.get_lr()[0], self.global_steps)
-                            self.writer.add_scalar('num_representatives', self.get_num_representatives(), self.global_steps)
-                            self.writer.add_scalar('num_representatives', self.get_num_representatives(), self.global_steps)
-                        self.writer.flush()
-                    if self.watcher is not None:
-                        self.observe(trainer=self,
-                                    model=self.model,
-                                    optimizer=self.optimizer,
-                                    data=(x, y))
-                        self.stream_meters(meters, prefix=prefix)
-                        if training:
-                            self.write_stream('lr',
-                                            (self.global_steps, self.optimizer.get_lr()[0]))
-                torch.cuda.nvtx.range_pop()
-                step_count += 1
-            end = time.time()
+                if self.writer is not None:
+                    self.writer.add_scalar(f"{prefix}_loss", meters['loss'].avg, self.global_steps)
+                    self.writer.add_scalar(f"{prefix}_prec1", meters['prec1'].avg, self.global_steps)
+                    self.writer.add_scalar(f"{prefix}_prec5", meters['prec5'].avg, self.global_steps)
+                    if training:
+                        self.writer.add_scalar('lr', self.optimizer.get_lr()[0], self.global_steps)
+                        self.writer.add_scalar('num_representatives', self.get_num_representatives(), self.global_steps)
+                        self.writer.add_scalar('num_representatives', self.get_num_representatives(), self.global_steps)
+                    self.writer.flush()
+                if self.watcher is not None:
+                    self.observe(trainer=self,
+                                model=self.model,
+                                optimizer=self.optimizer,
+                                data=(x, y))
+                    self.stream_meters(meters, prefix=prefix)
+                    if training:
+                        self.write_stream('lr',
+                                        (self.global_steps, self.optimizer.get_lr()[0]))
+            torch.cuda.nvtx.range_pop()
+            step_count += 1
+        end = time.time()
 
-            meters = {name: meter.avg.item() for name, meter in meters.items()}
-            meters['error1'] = 100. - meters['prec1']
-            meters['error5'] = 100. - meters['prec5']
-            meters['time'] = end - start
-            meters['step_count'] = step_count
+        meters = {name: meter.avg.item() for name, meter in meters.items()}
+        meters['error1'] = 100. - meters['prec1']
+        meters['error5'] = 100. - meters['prec5']
+        meters['time'] = end - start
+        meters['step_count'] = step_count
 
-            return meters
+        return meters
 
     def _step(self, i_batch, inputs_batch, target_batch, training=False,
               average_output=False, chunk_batch=1):
@@ -270,8 +263,11 @@ class nil_global_agent(Agent):
 
             if training:
                 # Get the representatives
-                rep_values, rep_labels, rep_weights = self.get_candidates()
+                rep_values, rep_labels, rep_weights = self.get_representatives()
                 num_reps = len(rep_values)
+                if self.writer is not None and num_reps > 0:
+                    grid = torchvision.utils.make_grid(rep_values)
+                    self.writer.add_image("rep_values", grid)
 
             # Get the respective weights
             w = torch.ones(len(x))
