73e6710b75268458507d3d4e46ddbb66bac9554d
Thomas Bouvier <contact@thomas-bouvier.io>
2022-04-21 17:33:01 +0200

    feat(cpp): add fake dataloader

--- cpp-loader/torch_test.py
+++ cpp-loader/torch_test.py
@@ -2,9 +2,39 @@ import torch
 import rehearsal
 import random
 import ctypes
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+
+class MyDataset(Dataset):
+    def __init__(self, values, labels):
+        super(MyDataset, self).__init__()
+        self.values = values
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, index):
+        return self.values[index], self.labels[index]
+
 
 if __name__ == "__main__":
-    l = [(random.choice('abcdefghijklmnopqrstuvwxyz'), torch.rand(4)) for i in range(0, 1000)]
-    sl = rehearsal.StreamLoader(26, 10, ctypes.c_int64(torch.random.initial_seed()).value)
-    sl.accumulate(l)
-    print(sl.get_samples(10))
+    torch.manual_seed(0)
+    random.seed(0)
+    np.random.seed(0)
+
+    values = np.random.rand(5000, 3, 32, 32)
+    labels = np.random.rand(5000)
+    dataset = MyDataset(values, labels)
+    loader = DataLoader(dataset=dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True)
+
+    sl = rehearsal.StreamLoader(26, 10, ctypes.c_int64(
+        torch.random.initial_seed()).value)
+
+    for epoch in range(10):
+        for inputs, target in loader:
+            # inputs shape: torch.Size([128, 3, 32, 32])
+            # target shape: torch.Size([128])
+            pass
+            #sl.accumulate((inputs, target))
