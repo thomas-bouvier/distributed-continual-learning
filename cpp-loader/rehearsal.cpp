8d736ca5762c91c9cda6df16b1cbd677180754ac
Bogdan Nicolae <bogdan.nicolae@acm.org>
2022-04-21 19:25:17 -0500

    async rehearsal buffer update

--- cpp-loader/rehearsal.cpp
+++ cpp-loader/rehearsal.cpp
@@ -9,5 +9,5 @@ PYBIND11_MODULE(rehearsal, m) {
     py::class_<stream_loader_t>(m, "StreamLoader")
         .def(py::init<unsigned int, unsigned int, int64_t>())
 	.def("accumulate", &stream_loader_t::accumulate)
-	.def("get_samples", &stream_loader_t::get_samples);
+	.def("wait", &stream_loader_t::wait);
 }
--- cpp-loader/torch_test.py
+++ cpp-loader/torch_test.py
@@ -3,8 +3,31 @@ import rehearsal
 import random
 import ctypes
 
+K = 5
+N = 10
+R = 2
+
+def random_batch():
+    samples = torch.rand(N, 3)
+    labels = torch.randint(high=K, size=(N,))
+    return (samples, labels)
+
+
 if __name__ == "__main__":
-    l = [(random.choice('abcdefghijklmnopqrstuvwxyz'), torch.rand(4)) for i in range(0, 1000)]
-    sl = rehearsal.StreamLoader(26, 10, ctypes.c_int64(torch.random.initial_seed()).value)
-    sl.accumulate(l)
-    print(sl.get_samples(10))
+    sl = rehearsal.StreamLoader(K, N // 2, ctypes.c_int64(torch.random.initial_seed()).value)
+
+    aug_samples = torch.zeros(N + R, 3)
+    aug_labels = torch.randint(high=K, size=(N + R,))
+    aug_weights = torch.zeros(N + R)
+
+    print("Round 1")
+    samples, labels = random_batch()
+    sl.accumulate(samples, labels, aug_samples, aug_labels, aug_weights)
+    size = sl.wait()
+    print(size, samples, aug_samples)
+
+    print("Round 2")
+    samples, labels = random_batch()
+    sl.accumulate(samples, labels, aug_samples, aug_labels, aug_weights)
+    size = sl.wait()
+    print(size, samples, aug_samples)
