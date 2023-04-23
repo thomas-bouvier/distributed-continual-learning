fix: loss calculation

agents/base.py
@@ -238,21 +238,22 @@ class Agent:
             else:
                 output = self.model(x)
                 loss = self.criterion(output, y)
+            loss = loss.sum() / self.mask[y].sum()
 
             assert not torch.isnan(loss).any(), "Loss is NaN, stopping training"
 
             if self.use_amp:
-                self.scaler.scale(loss).backward(torch.ones_like(loss))
+                self.scaler.scale(loss).backward()
                 self.optimizer_regime.optimizer.synchronize()
                 with self.optimizer_regime.optimizer.skip_synchronize():
                     self.scaler.step(self.optimizer_regime.optimizer)
                     self.scaler.update()
             else:
-                loss.backward(torch.ones_like(loss))
+                loss.backward()
                 self.optimizer_regime.step()
 
             prec1, prec5 = accuracy(output, y, topk=(1, 5))
-            meters["loss"].update(loss.sum() / self.mask[y].sum(), x.size(0))
+            meters["loss"].update(loss)
             meters["prec1"].update(prec1, x.size(0))
             meters["prec5"].update(prec5, x.size(0))
             meters["num_samples"].update(self.batch_size)

agents/nil_cpp.py
@@ -324,7 +324,7 @@ class nil_cpp_agent(Agent):
 
             # https://stackoverflow.com/questions/43451125/pytorch-what-are-the-gradient-arguments
             total_weight = hvd.allreduce(torch.sum(new_w), name='total_weight', op=hvd.Sum)
-            dw = new_w / total_weight
+            dw = new_w / total_weight * self.batch_size * hvd.size() / self.batch_size
 
             if self.use_amp:
                 self.scaler.scale(loss).backward(dw)
@@ -337,14 +337,13 @@ class nil_cpp_agent(Agent):
                 self.optimizer_regime.step()
 
             self.current_rehearsal_size = self.dsl.get_rehearsal_size()
-            meters["num_samples"].update(self.aug_size)
 
             # measure accuracy and record loss
             prec1, prec5 = accuracy(output, new_y, topk=(1, 5))
-            # https://discuss.pytorch.org/t/passing-the-weights-to-crossentropyloss-correctly/14731/10
-            meters["loss"].update(loss.sum() / self.mask[new_y].sum(), new_x.size(0))
+            meters["loss"].update(loss.sum() / self.mask[new_y].sum())
             meters["prec1"].update(prec1, new_x.size(0))
             meters["prec5"].update(prec5, new_x.size(0))
+            meters["num_samples"].update(self.aug_size)
 
     def validate_one_epoch(self, data_regime, previous_task=False):
         prefix = "val"