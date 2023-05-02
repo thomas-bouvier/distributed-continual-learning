fix: broadcast current optimizer state

optimizer_regime.py
@@ -122,7 +122,8 @@ class OptimizerRegime(Regime):
     def reset(self, parameters):
         logging.debug("OPTIMIZER REGIME - resetting state..")
         self.optimizer.load_state_dict(
-            torch.optim.SGD(parameters, lr=0).state_dict())
+            _OPTIMIZERS[self.config.get("optimizer", "SGD")](parameters, lr=0).state_dict()
+        )
         # Horovod: broadcast optimizer state.
         hvd.broadcast_optimizer_state(self.optimizer, root_rank=0)
         self.config = self.defaults