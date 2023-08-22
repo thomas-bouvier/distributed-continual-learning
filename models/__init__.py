cb753a9a36f5d2e8f716a890ee13880db03326f8
Malvin Chevallier <malvin.chevallier@gmail.com>
2023-08-22 14:22:21 +0200

    fix: modif __init__

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .vanilla import *
 from .agem import *
 from .der import *
 from .derpp import *
+from .hal import *
--- models/hal.py
+++ models/hal.py
@@ -41,6 +41,13 @@ class Hal(ContinualLearner):
             buffer_config,
             batch_metrics,
         )
+        try:
+            self.gamma = config["gamma"]
+            self.beta = config["beta"]
+            self.hal_lambda = config["hal_lambda"]
+            self.lr = config["lr"]
+        except:
+            raise Exception("Parameters beta, gamma and hal lambda is required for HAL")
 
         self.use_memory_buffer = True
         self.finetuning_epochs = 1
@@ -49,11 +56,11 @@ class Hal(ContinualLearner):
         self.y = None
         self.spare_model = copy.deepcopy(self.backbone)
         # self.spare_model.to(device)
-        self.spare_opt = SGD(self.spare_model.parameters(), lr=0.1)
+        self.spare_opt = SGD(self.spare_model.parameters(), lr=self.lr)
         self.anchor_optimization_steps = 100
-        self.gamma = 0.1
-        self.beta = 0.5
-        self.hal_lambda = 0.1
+        # self.gamma = 0.1
+        # self.beta = 0.5
+        # self.hal_lambda = 0.1
 
     def before_all_tasks(self, train_data_regime):
         self.buffer = Buffer(
@@ -75,22 +82,48 @@ class Hal(ContinualLearner):
 
     def after_every_task(self, task_id, train_data_regime):
         self.get_anchors(task_id, train_data_regime)
+        del self.phi
+
+    def get_params(self, backbone) -> torch.Tensor:
+        """
+        Returns all the parameters concatenated in a single tensor.
+        :return: parameters tensor (??)
+        """
+        params = []
+        for pp in list(backbone.parameters()):
+            params.append(pp.view(-1))
+        return torch.cat(params)
+
+    def set_params(self, backbone, new_params: torch.Tensor) -> None:
+        """
+        Sets the parameters to a given value.
+        :param new_params: concatenated values to be set (??)
+        """
+        progress = 0
+        for pp in list(backbone.parameters()):
+            cand_params = new_params[
+                progress : progress + torch.tensor(pp.size()).prod()
+            ].view(pp.size())
+            progress += torch.tensor(pp.size()).prod()
+            pp.data = cand_params
 
     def get_anchors(self, task_id, train_data_regime):
         # params = self.backbone.parameters()
         # theta_t = [x.detach().clone() for x in params]
-        theta_t = self.backbone.state_dict()  # state_dict au lieu de parameters ?
-        self.spare_model.load_state_dict(theta_t)
+        # theta_t = self.backbone.state_dict()  # state_dict au lieu de parameters ?
+        # self.spare_model.load_state_dict(theta_t)
+        theta_t = self.get_params(self.backbone).detach().clone()
+        self.set_params(self.spare_model, theta_t)
 
         for _ in range(self.finetuning_epochs):  # Fine Tune
-            inputs, labels = self.x, self.y
+            inputs, labels = self.x.clone(), self.y.clone()
             self.spare_opt.zero_grad()
             out = self.spare_model(inputs)
             loss = self.criterion(out, labels)
             (loss.sum() / loss.size(0)).backward()
             self.spare_opt.step()
 
-        theta_m = self.spare_model.state_dict()
+        theta_m = self.get_params(self.backbone).detach().clone()
 
         classes_for_this_task = torch.where(
             torch.tensor(train_data_regime.classes_mask)
@@ -100,13 +133,13 @@ class Hal(ContinualLearner):
             e_t = torch.rand(
                 torch.Size([2] + list(self.x.shape[1:])), requires_grad=True
             )  # device=self.device
-            e_t_opt = SGD([e_t], lr=0.1)
+            e_t_opt = SGD([e_t], lr=self.lr)
             for i in range(self.anchor_optimization_steps):
                 e_t_opt.zero_grad()
                 cum_loss = 0
 
                 self.spare_opt.zero_grad()
-                self.spare_model.load_state_dict(theta_m)
+                self.set_params(self.spare_model, theta_m.detach().clone())
                 loss = -torch.sum(
                     self.criterion(
                         self.spare_model(e_t), torch.full((2,), a_class.item())
@@ -116,7 +149,7 @@ class Hal(ContinualLearner):
                 cum_loss += loss.item()
 
                 self.spare_opt.zero_grad()
-                self.spare_model.load_state_dict(theta_m)
+                self.set_params(self.spare_model, theta_m.detach().clone())
                 loss = torch.sum(
                     self.criterion(
                         self.spare_model(e_t), torch.full((2,), a_class.item())
@@ -128,7 +161,7 @@ class Hal(ContinualLearner):
                 self.spare_opt.zero_grad()
                 loss = torch.sum(
                     self.gamma
-                    * (self.spare_model.forward_features(e_t) - self.phi[:2]) ** 2
+                    * (self.spare_model.forward_features(e_t)[0] - self.phi[0]) ** 2
                 )
                 assert not self.phi.requires_grad
                 loss.backward()
@@ -163,7 +196,7 @@ class Hal(ContinualLearner):
                 tuple([0] + list(torch.Size(list(self.x.shape[1:]))))
             )  # .to(self.device)
 
-        old_weights = self.backbone.state_dict()
+        old_weights = self.get_params(self.backbone).detach().clone()
 
         with get_timer(
             "train",
@@ -177,7 +210,7 @@ class Hal(ContinualLearner):
             # Forward pass
             with autocast(enabled=self.use_amp):
                 output = self.backbone(aug_x)
-                loss = self.criterion(output, aug_y)
+                firstloss = self.criterion(output, aug_y)
 
             # TODO: if true for multiple iterations, trigger this
             # assert not torch.isnan(loss).any(), "Loss is NaN, stopping training"
@@ -189,19 +222,18 @@ class Hal(ContinualLearner):
             dw = aug_w / total_weight * self.batch_size * hvd.size() / self.batch_size
 
             # Backward pass
-            self.scaler.scale(loss.sum() / loss.size(0)).backward()
+            self.scaler.scale(firstloss.sum() / firstloss.size(0)).backward()
             self.optimizer_regime.optimizer.synchronize()
             with self.optimizer_regime.optimizer.skip_synchronize():
                 self.scaler.step(self.optimizer_regime.optimizer)
                 self.scaler.update()
 
             if len(self.anchors) > 0:
-                pred_anchors = 0
                 anchors_points = torch.tensor(1)
                 with torch.no_grad():
-                    pred_anchors += self.backbone(self.anchors)
+                    pred_anchors = self.backbone(self.anchors)
 
-                self.backbone.load_state_dict(old_weights)
+                self.set_params(self.backbone, old_weights)
                 pred_anchors -= self.backbone(self.anchors)
                 loss = self.hal_lambda * (pred_anchors**2).mean()
                 loss.backward()
@@ -214,7 +246,7 @@ class Hal(ContinualLearner):
 
             # Measure accuracy and record metrics
             prec1, prec5 = accuracy(output, aug_y, topk=(1, 5))
-            meters["loss"].update(loss.sum() / aug_y.size(0))
+            meters["loss"].update(firstloss.sum() / aug_y.size(0))
             meters["prec1"].update(prec1, aug_x.size(0))
             meters["prec5"].update(prec5, aug_x.size(0))
             meters["num_samples"].update(aug_x.size(0))
